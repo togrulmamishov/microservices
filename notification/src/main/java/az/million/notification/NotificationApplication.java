8b95f85366ed13e95d4e6e6aaf6b6732e02718ee
togrul.mamishov <togrul511@gmail.com>
2022-05-20 17:40:13 +0400

    RabbitMQ implemented

--- notification/src/main/java/az/million/notification/NotificationApplication.java
+++ notification/src/main/java/az/million/notification/NotificationApplication.java
@@ -1,11 +1,20 @@
 package az.million.notification;
 
+import az.million.amqp.RabbitMQMessageProducer;
+import az.million.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "az.million.notification",
+                "az.million.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "az.million.clients"
@@ -14,5 +23,14 @@ public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+                                        NotificationConfig notificationConfig) {
+        return args -> producer.publish(
+                "foo",
+                notificationConfig.getInternalExchange(),
+                notificationConfig.getInternalNotificationRoutingKey());
     }
 }
